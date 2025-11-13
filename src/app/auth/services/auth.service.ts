f4d718160da421f25e879071adeaaef3313b6a34
Anthony Alarcón <ara.alarcon@yavirac.edu.ec>
2025-11-12 22:53:35 -0500

    guia 45

---
+++ src/app/auth/services/auth.service.ts
@@ -0,0 +1,22 @@
+import { Injectable, inject } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AuthService {
+
+  private baseUrl="http://127.0.0.1:3000"
+
+  private http=inject(HttpClient)
+
+
+  constructor() { }
+  loginConNest(credenciales: any){
+    return this.http.post<any>(`${this.baseUrl}/auth/login`, credenciales);
+  }
+
+  registroConNest(datos: any){
+    return this.http.post<any>(`${this.baseUrl}/register`, datos);
+  }
+}
